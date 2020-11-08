bce1f8856a947116675496aa87410a9fc3e0326f
Lauty289 <lautaroramirez289@gmail.com>
2020-11-08 20:14:04 -0300

    components/gasto y bootswatch

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GastoComponent } from './components/gasto/gasto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GastoComponent
   ],
   imports: [
     BrowserModule,
