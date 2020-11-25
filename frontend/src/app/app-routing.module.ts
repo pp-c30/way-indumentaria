630916b062d64816837c0b082b2b95181314582f
Agus019 <agustin.ivan.gonzalez.20.08.2001@gmail.com>
2020-11-25 17:43:26 -0300

    Creacion de rutas para el menu

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CategoriaGastosComponent } from "./components/categoria-gastos/categoria-gastos.component";
+import { GastosComponent } from "./components/gastos/gastos.component";
+import { LocalidadesComponent } from "./components/localidades/localidades.component";
+import { ProvinciasComponent } from "./components/provincias/provincias.component";
+import { VentasComponent } from "./components/ventas/ventas.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"categoriaGastos",component:CategoriaGastosComponent
+  },
+  {
+    path:"gastos",component:GastosComponent
+  },
+  {
+    path:"localidades",component:LocalidadesComponent
+  },
+  {
+    path:"provincias",component:ProvinciasComponent
+  },
+  {
+    path:"ventas",component:VentasComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
