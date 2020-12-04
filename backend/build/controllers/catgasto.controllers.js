ce7fa01dc6bcb300a9e0b3bdbe0be9e5d537720f
Lauty289 <lautaroramirez289@gmail.com>
2020-12-04 17:19:40 -0300

    post vendedor

--- backend/build/controllers/catgasto.controllers.js
+++ backend/build/controllers/catgasto.controllers.js
@@ -9,6 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.CatgastoController = void 0;
 const database_1 = require("../routes/database");
 // CRUD
 class CatgastoController {
--- frontend/src/app/components/vendedores/vendedores.component.ts
+++ frontend/src/app/components/vendedores/vendedores.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { VendedoresService } from "../../services/vendedores.service";
+import { FormBuilder , FormGroup } from "@angular/forms";
+import { LocalidadesService } from "../../services/localidades.service";
 
 @Component({
   selector: 'app-vendedores',
@@ -9,17 +11,63 @@ import { VendedoresService } from "../../services/vendedores.service";
 export class VendedoresComponent implements OnInit {
 
   listVendedores = [];
+  formVendedor: FormGroup;
+  listVendedoresLoc = [];
 
-  constructor(private vendedoresServ:VendedoresService) { }
+  constructor(private vendedoresServ:VendedoresService, private fb: FormBuilder, private vendedorLocServ:LocalidadesService ) {
+
+    this.formVendedor = this.fb.group({
+      
+      // descripcion:['']
+      nombre:[''],
+      apellido:[''],
+      dni:[''],
+      domicilio:[''],
+      email:[''],
+      localidad:[0],
+      adjunto:[''],
+      telefono:[''],
+      nom_garante:[''],
+      ape_garante:[''],
+      email_garante:[''],
+      dni_garante:[''],
+      domicilio_garante:[''],
+      telefono_garante:[''],
+      estado:['']
+
+    })
+
+   }
 
   ngOnInit(): void {
     this.obtenerVendedores();
+    this.obtenerLocalidades();
   }
 
   obtenerVendedores(){
     this.vendedoresServ.getVendedores().subscribe(
       resultado => this.listVendedores = resultado,
       error => console.log(error)
+    )
+  }
+
+  guardarVendedores(){
+    // console.log(this.formProvincia.value);
+    this.vendedoresServ.saveVendedores(this.formVendedor.value).subscribe(
+      resultado => {
+        console.log(resultado);
+        this.obtenerVendedores();//se refresca la grilla
+        this.formVendedor.reset();
+        this.formVendedor.get('localidad').setValue(0);
+      },
+      error => console.log(error)
+
+    )
+
+  }
+  obtenerLocalidades(){
+    this.vendedorLocServ.getLocalidades().subscribe(
+      resultado => this.listVendedoresLoc = resultado
     )
   }
 
