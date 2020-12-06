3add82d1991f1d037a9eca4431aa52f9cbb1ab32
Agus019 <agustin.ivan.gonzalez.20.08.2001@gmail.com>
2020-12-06 17:29:15 -0300

    Componente y servicio de venta_impaga_paga

---
+++ frontend/src/app/components/ventas-impagas-pagas/ventas-impagas-pagas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ventas-impagas-pagas',
+  templateUrl: './ventas-impagas-pagas.component.html',
+  styleUrls: ['./ventas-impagas-pagas.component.css']
+})
+export class VentasImpagasPagasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
