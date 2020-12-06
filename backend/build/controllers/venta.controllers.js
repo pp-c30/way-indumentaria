10b4a72f8c996194f2fa97731f9efed1eeecbc29
Agus019 <agustin.ivan.gonzalez.20.08.2001@gmail.com>
2020-12-06 16:25:22 -0300

    Compilacion de archivos en el backend

--- backend/build/controllers/venta.controllers.js
+++ backend/build/controllers/venta.controllers.js
@@ -9,7 +9,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ventaController = void 0;
 const database_1 = require("../routes/database");
 class ventaController {
     listaVentas(req, res) {
