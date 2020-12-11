5fc4ac7eac66cf686a563cd308453be004567865
Lauty289 <lautaroramirez289@gmail.com>
2020-12-11 19:48:18 -0300

    list y form categoria

--- frontend/src/app/components/categorias/categorias.component.ts
+++ frontend/src/app/components/categorias/categorias.component.ts
@@ -1,15 +1,107 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CategoriasService } from "../../services/categorias.service";
+import { FormBuilder , FormGroup } from "@angular/forms";
+import { ICategoria } from "src/app/models/categoria";
+
 @Component({
   selector: 'app-categorias',
   templateUrl: './categorias.component.html',
   styleUrls: ['./categorias.component.css']
 })
 export class CategoriasComponent implements OnInit {
+  listCategoria = [];
 
-  constructor() { }
+  formCategoria: FormGroup;
+
+  constructor( private categoriaServ: CategoriasService, private fb: FormBuilder ) { 
+
+    this.formCategoria = this.fb.group({
+      id_categoria:[''],
+      descripcion:[''],
+      estado:['']
+
+
+    })
+
+   }
 
   ngOnInit(): void {
+    this.obtenerCategoria();
   }
+
+  obtenerCategoria(){
+    this.categoriaServ.getCategoria().subscribe(
+
+        resultado => this.listCategoria = resultado,
+        error => console.log(error)
+         
+    )
+  }
+
+  guardarCategoria(){
+    // console.log(this.formProvincia.value);
+    if (this.formCategoria.value.id_categoria){
+
+         // si existe el id se actualiza
+     this.categoriaServ.updateCategoria(this.formCategoria.value).subscribe(
+      respuesta => {
+        console.log(respuesta);
+        this.obtenerCategoria();
+        this.formCategoria.reset();
+      },
+      error => console.log(error)
+  )
+    }else{
+    
+    this.categoriaServ.saveCategoria(this.formCategoria.value).subscribe(
+      resultado => {
+        console.log(resultado);
+        this.obtenerCategoria();//se refresca la grilla
+        this.formCategoria.reset();
+      },
+      error => console.log(error)
+
+    )
+  }
+
+  }
+
+  editarCategoria(categoria:ICategoria){
+    this.formCategoria.setValue({
+      id_categoria:categoria.id_categoria,
+      descripcion:categoria.descripcion,
+
+    });
+    
+     }
+
+     eliminarCategoria(id:number){
+
+      if(confirm('¿Esta seguro que desea eliminar?')){
+  
+        this.categoriaServ.deleteCategoria(id).subscribe(
+          respuesta =>{
+            console.log(respuesta);
+            this.obtenerCategoria();
+    
+          },
+          error => console.log(error)
+          
+        )
+      }
+  
+  
+      
+    }
+
+  
+
+
+
+     resetearformCategoria(){
+      this.formCategoria.reset();
+    }
+
 
 }
--- frontend/src/app/services/categorias.service.ts
+++ frontend/src/app/services/categorias.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { ICategoria } from "../models/categoria";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriasService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  getCategoria(){
+
+    return this.http.get<ICategoria[]>('http://localhost:3000/categoria');
+  }
+
+  saveCategoria(unaCategoria:ICategoria){
+
+    return this.http.post('http://localhost:3000/categoria', unaCategoria);
+    
+
+   }
+   updateCategoria(unaCategoria:ICategoria){
+
+    let id:number = unaCategoria.id_categoria;
+
+    return this.http.put('http://localhost:3000/categoria/'+id,unaCategoria);
+
+
+   }
+   deleteCategoria(id:number){
+
+    return this.http.delete('http://localhost:3000/categoria/' +id);
+   }
+
+
+
+
+
+
+
 }
