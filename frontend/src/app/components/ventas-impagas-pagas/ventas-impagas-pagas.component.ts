be1b1ecc0a4e84ca920601044a6a46c8ea7cb1a3
Lauty289 <lautaroramirez289@gmail.com>
2020-12-15 21:34:37 -0300

    post y get ventas_impaga_paga

--- frontend/src/app/components/ventas-impagas-pagas/ventas-impagas-pagas.component.ts
+++ frontend/src/app/components/ventas-impagas-pagas/ventas-impagas-pagas.component.ts
@@ -1,15 +1,108 @@
 import { Component, OnInit } from '@angular/core';
 
+import { VentasImpagasPagasService } from "../../services/ventas-impagas-pagas.service";
+import { FormBuilder , FormGroup } from "@angular/forms";
+import { IVentaImpagaPaga } from "src/app/models/venta_impaga_paga";
+
 @Component({
   selector: 'app-ventas-impagas-pagas',
   templateUrl: './ventas-impagas-pagas.component.html',
   styleUrls: ['./ventas-impagas-pagas.component.css']
 })
 export class VentasImpagasPagasComponent implements OnInit {
+  listVentaImpagaPaga = [];
 
-  constructor() { }
+  formVentaImpagaPaga: FormGroup;
+
+  constructor( private ventaImpagaPagaServ: VentasImpagasPagasService, private fb: FormBuilder ) { 
+    this.formVentaImpagaPaga = this.fb.group({
+      id_impaga_paga:[''],
+      fecha_carga:[''],
+      vendedor:[''],
+      total:[''],
+      debe:[''],
+      estado:['']
+
+
+    })
+
+
+
+   }
 
   ngOnInit(): void {
+    this.obtenerVentaImpagaPaga();
   }
 
+  obtenerVentaImpagaPaga(){
+    this.ventaImpagaPagaServ.getVentaImpagaPaga().subscribe(
+
+        resultado => this.listVentaImpagaPaga = resultado,
+        error => console.log(error)
+         
+    )
+  }
+
+  guardarVentaImpagaPaga(){
+    // console.log(this.formProvincia.value);
+    if (this.formVentaImpagaPaga.value.id_impaga_paga){
+
+         // si existe el id se actualiza
+     this.ventaImpagaPagaServ.updateVentaImpagaPaga(this.formVentaImpagaPaga.value).subscribe(
+      respuesta => {
+        console.log(respuesta);
+        this.obtenerVentaImpagaPaga();
+        this.formVentaImpagaPaga.reset();
+      },
+      error => console.log(error)
+  )
+    }else{
+    
+    this.ventaImpagaPagaServ.saveVentaImpagaPaga(this.formVentaImpagaPaga.value).subscribe(
+      resultado => {
+        console.log(resultado);
+        this.obtenerVentaImpagaPaga();//se refresca la grilla
+        this.formVentaImpagaPaga.reset();
+      },
+      error => console.log(error)
+
+    )
+  }
+
+ 
 }
+
+editarVentaImpagaPaga(venta_impaga_paga:IVentaImpagaPaga){
+  this.formVentaImpagaPaga.setValue({
+    id_impaga_paga:venta_impaga_paga.id_impaga_paga,
+    fecha_carga:venta_impaga_paga.fecha_carga,
+    vendedor:venta_impaga_paga.vendedor,
+    total:venta_impaga_paga.total,
+    debe:venta_impaga_paga.debe,
+    estado:venta_impaga_paga.estado,
+
+  });
+  
+   }
+
+   eliminarVentaImpagaPaga(id:number){
+
+    if(confirm('¿Esta seguro que desea eliminar?')){
+
+      this.ventaImpagaPagaServ.deleteVentaImpagaPaga(id).subscribe(
+        respuesta =>{
+          console.log(respuesta);
+          this.obtenerVentaImpagaPaga();
+  
+        },
+        error => console.log(error)
+        
+      )
+    }
+
+
+}
+
+resetearformVentaImpagaPaga(){
+  this.formVentaImpagaPaga.reset();
+}}
--- frontend/src/app/services/ventas-impagas-pagas.service.ts
+++ frontend/src/app/services/ventas-impagas-pagas.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { IVentaImpagaPaga } from "../models/venta_impaga_paga";
 
 @Injectable({
   providedIn: 'root'
 })
 export class VentasImpagasPagasService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  getVentaImpagaPaga(){
+
+    return this.http.get<IVentaImpagaPaga[]>('http://localhost:3000/venta_impaga_paga');
+  }
+
+  saveVentaImpagaPaga(unaVentaImpagaPaga:IVentaImpagaPaga){
+
+    return this.http.post('http://localhost:3000/venta_impaga_paga', unaVentaImpagaPaga);
+    
+
+   }
+   updateVentaImpagaPaga(unaVentaImpagaPaga:IVentaImpagaPaga){
+
+    let id:number = unaVentaImpagaPaga.id_impaga_paga;
+
+    return this.http.put('http://localhost:3000/venta_impaga_paga/'+id,unaVentaImpagaPaga);
+
+
+   }
+   deleteVentaImpagaPaga(id:number){
+
+    return this.http.delete('http://localhost:3000/venta_impaga_paga/' +id);
+   }
+
 }
