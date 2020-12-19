bc81714846bc666b5224b654c10299fe8e785bcd
Agus019 <agustin.ivan.gonzalez.20.08.2001@gmail.com>
2020-12-19 17:04:12 -0300

    Instalacion de dotenv y de los types

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -18,8 +18,6 @@ import { ProductosComponent } from './components/productos/productos.component';
 import { VentasImpagasPagasComponent } from './components/ventas-impagas-pagas/ventas-impagas-pagas.component';
 import { CategoriasComponent } from './components/categorias/categorias.component';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 
 @NgModule({
   declarations: [
